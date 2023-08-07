c52901a26715922f2277edb47916eb1b6f183b8a
learsiOtni <learsiOtni@gmail.com>
2023-08-07 23:09:57 +0100

    project start

---
+++ data.ts
@@ -0,0 +1,166 @@
+export const statistics = [
+    {
+        id: 1,
+        title: "Status Updates",
+        currentValue: 50,
+        previousValue: 45,
+    },
+    {
+        id: 2,
+        title: "Projects",
+        currentValue: 24,
+        previousValue: 28,
+    },
+    {
+        id: 3,
+        title: "Tags",
+        currentValue: 156,
+    },
+    {
+        id: 4,
+        title: "Users",
+        currentValue: 86,
+    },
+
+]
+
+
+export const statusUpdates = [
+    {
+        statusId: 1,
+        title: "Performance - GetUserDetails request is slow",
+        body: "The Performance details takes too long to load which leaves the user with... empty field screen. There are some screen in the",
+        user: {
+            userId: 123,
+            name: "Time Stones"
+        },
+        createdAt: "5 mins ago",
+        tags: [
+            "Subscription",
+            "Trial"
+        ]
+    },
+    {
+        statusId: 2,
+        title: "Performance - GetUserDetails request is slow",
+        body: "The Performance details takes too long to load which leaves the user with... empty field screen. There are some screen in the",
+        user: {
+            userId: 123,
+            name: "Time Stones"
+        },
+        createdAt: "10 mins ago",
+        tags: [
+            "Subscription",
+            "Trial"
+        ]
+    },
+    {
+        statusId: 3,
+        title: "Performance - GetUserDetails request is slow",
+        body: "The Performance details takes too long to load which leaves the user with... empty field screen. There are some screen in the",
+        user: {
+            userId: 123,
+            name: "Time Stones"
+        },
+        createdAt: "20 mins ago",
+        tags: [
+            "Subscription",
+            "Trial"
+        ]
+    },
+    {
+        statusId: 4,
+        title: "Performance - GetUserDetails request is slow",
+        body: "The Performance details takes too long to load which leaves the user with... empty field screen. There are some screen in the",
+        user: {
+            userId: 123,
+            name: "Time Stones"
+        },
+        createdAt: "1 hour ago",
+        tags: [
+            "Subscription",
+            "Trial"
+        ]
+    }
+]
+
+export const Projects = [
+    {
+        id: 1,
+        title: "Project Twitter",
+        body: "lorem posum",
+        user: {
+            userId: 123,
+            name: "Time Stones"
+        },
+        createdAt: "",
+        teamMembers: [
+            "Jane Dayle",
+            "John Doe"
+        ]
+    },
+    {
+        id: 2,
+        title: "Project Twitter",
+        body: "lorem posum",
+        user: {
+            userId: 123,
+            name: "Time Stones"
+        },
+        createdAt: "",
+        teamMembers: [
+            "Jane Dayle",
+            "John Doe"
+        ]
+    },
+    {
+        id: 3,
+        title: "Project Twitter",
+        body: "lorem posum",
+        user: {
+            userId: 123,
+            name: "Time Stones"
+        },
+        createdAt: "",
+        teamMembers: [
+            "Jane Dayle",
+            "John Doe"
+        ]
+    },
+    {
+        id: 4,
+        title: "Project Twitter",
+        body: "lorem posum",
+        user: {
+            userId: 123,
+            name: "Time Stones"
+        },
+        createdAt: "",
+        teamMembers: [
+            "Jane Dayle",
+            "John Doe"
+        ]
+    }
+]
+
+export const Users = [
+    {
+        id: 1,
+        name: "Isaac Newton",
+        role: "Senior Project Leader",
+        updates: [
+            "ProjectX",
+            "ProjectY",
+        ],
+        teams: [
+            "ProjectX",
+            "ProjectY",
+        ],
+        projects: [
+            "ProjectX",
+            "ProjectY",
+        ]
+    }
+]
+
+
